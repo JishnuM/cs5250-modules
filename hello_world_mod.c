c6672b4e01ed147720866905f850c11e89db50e8
Jishnu Mohan <mohanjishnu@gmail.com>
2019-03-28 10:23:46 -0500

    Add a who parameter

--- hello_world_mod.c
+++ hello_world_mod.c
@@ -1,11 +1,17 @@
 #include <linux/kernel.h>
 #include <linux/init.h>
 #include <linux/module.h>
+#include <linux/moduleparam.h>
 
 MODULE_LICENSE("GPL");
 
+static char *who = "someone";
+
+module_param(who, charp, S_IRUSR | S_IWUSR);
+MODULE_PARM_DESC(who, "Who should I say hello to");
+
 static int hello_init(void) {
-	printk(KERN_ALERT "Hello world\n");
+	printk(KERN_ALERT "Hello world %s\n", who);
 	return 0;
 }
 
